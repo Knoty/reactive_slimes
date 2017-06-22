ff1d94ea95b469e1d9d4744da73524fd12f80d5c
Rodion Zyuzev <Knoty01@gmail.com>
2017-06-22 19:39:26 +0300

    Fixed header.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Slimes Rumble</h2>
         </div>
         <Slime />
         <CreateButton />
