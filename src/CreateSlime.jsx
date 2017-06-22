1ea1cc4a9733296a9dc0a49bea9f9f0e5cd0a278
Rodion Zyuzev <Knoty01@gmail.com>
2017-06-22 20:19:42 +0300

    Added example picture for "Create new slime" button.

--- src/CreateSlime.jsx
+++ src/CreateSlime.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import addSlime from './add.png';
 
 function CreateSlimeButton(props) {
   return (
     <div className="create_slime_button" >
       <button onClick={props.onClickFunction} onMouseOver={props.onMouseOver}>
-        <img alt={`Create new slime`} src={`./logo.svg`} />
+        <img alt={`Create new slime`} src={addSlime} />
       </button>
       <pre>{`${props.currentPoolAmount}`}</pre>
     </div>
