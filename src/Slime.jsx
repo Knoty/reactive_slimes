9293e6e1f1184e5b3a1c46cc6dcce78c123d2455
Rodion Zyuzev <Knoty01@gmail.com>
2017-06-22 20:07:44 +0300

    Added example picture for slimes.

--- src/Slime.jsx
+++ src/Slime.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import poring from './pink_poring.png';
 
 function Slime(props) {
   return (
     <div className="slime" >
       <button onClick={props.onClickFunction} onMouseOver={props.onMouseOver}>
-        <img alt={`Slime ${props.name}, id ${props.id}`} src={`./logo.svg`} />
+        <img alt={`Slime ${props.name}, id ${props.id}`} src={poring} />
       </button>
       <pre>{`${props.currentHP}/${props.maxHP}`}</pre>
     </div>
