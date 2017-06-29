84b6679985fc0081d46280af3ab5ccdea14e91d5
Rodion Zyuzev <Knoty01@gmail.com>
2017-06-29 18:05:39 +0300

    Moved img render from html/js to css.

--- src/CreateSlime.jsx
+++ src/CreateSlime.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import addSlime from './add.png';
 
 function CreateSlimeButton(props) {
   return (
-    <div className="create_slime_button" >
-      <button onClick={props.onClickFunction} onMouseOver={props.onMouseOver}>
-        <img alt={`Create new slime`} src={addSlime} />
-      </button>
+    <div title ="Create new slime" className="create_slime_button" onClick={props.onClickFunction}>
       <pre>{props.currentPoolAmount}</pre>
     </div>
   );
