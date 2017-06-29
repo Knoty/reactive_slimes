d6cb0e55a3875cdd7da47451fde1b9c49b8f5e4f
Rodion Zyuzev <Knoty01@gmail.com>
2017-06-29 11:34:39 +0300

    Added approximate frame for state.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,26 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const state = {
+  slimes: {
+    1: {
+      name: 'name',
+      maxHP: 100,
+      currentHP: 90
+    },
+    2: {
+      name: 'name2',
+      maxHP: 100,
+      currentHP: 80
+    }
+  },
+  boss: {
+    maxHP: 100,
+    currentHP: 70,
+    damage: 40
+  },
+  poolAmount: 1000
+};
+
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
 registerServiceWorker();
