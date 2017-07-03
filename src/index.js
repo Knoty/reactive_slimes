5148b5c0475b79c9198901bd51510ccc598a4c74
Rodion Zyuzev <Knoty01@gmail.com>
2017-07-03 22:29:38 +0300

    Introduced Redux store provider to the application

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { ActionTypes } from "redux/es/createStore";
 
-const state = {
+const initialState = {
   slimes: [
     {
       id: 1,
       name: 'name',
-      maxHP: 100,
+      maxHP: 160,
       currentHP: 90
     },
     {
@@ -27,8 +30,30 @@ const state = {
   poolAmount: 1000
 };
 
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case 'hitSlime':
+      return state;
+    case 'hitBoss':
+      const bossStats = Object.assign({}, state.boss);
+      bossStats.currentHP -= action.damage;
+      return Object.assign({}, state, {boss: bossStats});
+    case 'heal':
+      return state;
+    case 'create':
+      return state;
+    case ActionTypes.INIT:
+      return state;
+    default:
+      throw new Error('invalid action: ' + action.type);
+  }
+};
+
 ReactDOM.render(
-  <App slimes={state.slimes} boss={state.boss} poolAmount={state.poolAmount} />,
+  <Provider store={createStore(reducer, initialState)}>
+    <App slimes={initialState.slimes} poolAmount={initialState.poolAmount} />
+  </Provider>,
   document.getElementById('root')
 );
 
