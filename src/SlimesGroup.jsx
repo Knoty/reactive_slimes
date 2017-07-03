4f537b03630137b321ee35f3e58f8fcb1fdf6cf0
Rodion Zyuzev <Knoty01@gmail.com>
2017-07-03 22:30:16 +0300

    Added implementation of healing slimes by `setState`

--- src/SlimesGroup.jsx
+++ src/SlimesGroup.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Slime from './Slime.jsx';
 
-const SlimesGroup = (props) => (
-    <div>
-      {props.slimes.map((slime) => <Slime key={slime.id} {...slime} />)}
-    </div>
-);
+class SlimesGroup extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          slimes: props.slimes
+        };
+    }
+
+    healSlime(id) {
+        const healOnlySlimesOfId = function (slime) {
+            if (slime.id === id) {
+                const newSlimeState = Object.assign({}, slime);
+                newSlimeState.currentHP += Math.round((5 + Math.random() * 10));
+                return newSlimeState;
+            } else {
+                return slime;
+            }
+        };
+        this.setState(
+          {
+            slimes: this.state.slimes.map(healOnlySlimesOfId)
+          }
+        );
+    }
+
+    render () {
+        return (
+            <div>
+              {this.state.slimes.map((slime) => <Slime key={slime.id} onClickFunction={() => { this.healSlime(slime.id); } } {...slime} />)}
+            </div>
+        );
+    }
+}
 
 export default SlimesGroup;
