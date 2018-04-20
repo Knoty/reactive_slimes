facaa0eab02caf93f23d2bebb53915d98e1134d4
Rodion Zyuzev <Knoty01@gmail.com>
2018-04-20 15:21:17 +0300

    Destructuring incoming data for code optimization.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import SlimeGroup from './SlimesGroup.jsx';
 import CreateSlimeButton from './CreateSlimeButton.jsx';
 import Boss from './Boss.jsx';
 
-const App = (props) => (
+const App = (
+  { slimes, boss, poolAmount }
+) => (
   <div className="App">
     <div className="App-intro">
       <h2>Welcome to Slimes Rumble</h2>
     </div>
-    <SlimeGroup slimes={props.slimes} />
-    <CreateSlimeButton currentPoolAmount={props.poolAmount} maxPoolAmount={1000} onClickFunction={() => null} />
-    <Boss currentHP={props.boss.currentHP} maxHP={props.boss.maxHP} damage={props.boss.damage} />
+    <SlimeGroup slimes={slimes} />
+    <CreateSlimeButton currentPoolAmount={poolAmount} maxPoolAmount={1000} onClickFunction={() => null} />
+    <Boss currentHP={boss.currentHP} maxHP={boss.maxHP} damage={boss.damage} />
   </div>
 );
 
