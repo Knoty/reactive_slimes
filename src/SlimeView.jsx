ac5a94d4be977a06e84f53d769925575aaf1d1d7
Rodion Zyuzev <knoty01@gmail.com>
2018-05-11 14:45:00 +0300

    Code cleanup.

--- src/SlimeView.jsx
+++ src/SlimeView.jsx
@@ -4,7 +4,7 @@ import poring from './pink_poring.png';
 const SlimeView = props => (
     <div className="slime">
         <button onClick={props.onClick}>
-        <img alt={`Slime ${props.name}, id ${props.id}`} src={poring} />
+            <img alt={`Slime ${props.name}, id ${props.id}`} src={poring} />
         </button>
         <pre>{`${props.currentHP}/${props.maxHP}`}</pre>
     </div>
