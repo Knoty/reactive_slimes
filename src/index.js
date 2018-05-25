3db1df2d4f5c18337efe7a4de9827c31a045bcd3
Rodion Ziuzev <Knoty01@gmail.com>
2018-05-25 14:29:59 +0300

    Replaced slimes state to more relevant App.js.

--- src/index.js
+++ src/index.js
@@ -5,20 +5,6 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const state = {
-  slimes: [
-    {
-      id: 1,
-      name: 'name',
-      maxHP: 100,
-      currentHP: 90
-    },
-    {
-      id: 2,
-      name: 'name2',
-      maxHP: 100,
-      currentHP: 80
-    }
-  ],
   boss: {
     maxHP: 100,
     currentHP: 70,
