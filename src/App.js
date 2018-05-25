b9f4e18bb63183bc7d2d7143bbd3e5b4b9b7fd02
Knoty <Knoty01@gmail.com>
2018-05-25 14:16:05 +0300

    Added logic & state for CreateSlimeButton component.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,65 @@ import SlimeGroup from './SlimesGroup.jsx';
 import CreateSlimeButton from './CreateSlimeButton.jsx';
 import Boss from './Boss.jsx';
 
-const App = (
-  { slimes, boss, poolAmount }
-) => (
-  <div className="App">
-    <div className="App-intro">
-      <h2>Welcome to Slimes Rumble</h2>
-    </div>
-    <SlimeGroup slimes={slimes} />
-    <CreateSlimeButton currentPoolAmount={poolAmount} maxPoolAmount={1000} onClickFunction={() => null} />
-    <Boss currentHP={boss.currentHP} maxHP={boss.maxHP} damage={boss.damage} />
-  </div>
-);
+class App extends React.Component {
+    maxId;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            slimes: [
+                {
+                    id: 1,
+                    name: 'name',
+                    maxHP: 100,
+                    currentHP: 90
+                },
+                {
+                    id: 2,
+                    name: 'name2',
+                    maxHP: 100,
+                    currentHP: 80
+                }
+            ]
+        };
+
+        this.maxId = this.state.slimes.length;
+    }
+
+    onClick() {
+        ++this.maxId;
+        this.setState(
+            oldState => ({
+                 slimes: oldState.slimes.concat(
+                     [{ id: this.maxId, name: `name${this.maxId}`, maxHP: 100, currentHP: 75 }]
+                 )
+            })
+        )
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-intro">
+                    <h2>Welcome to Slimes Rumble</h2>
+                </div>
+                <SlimeGroup slimes={this.state.slimes} />
+                <CreateSlimeButton
+                    currentPoolAmount={this.props.poolAmount}
+                    maxPoolAmount={1000}
+
+                    onClick={
+                        () => {
+                            this.onClick()
+                        }
+                    }
+
+                />
+                <Boss currentHP={this.props.boss.currentHP} maxHP={this.props.boss.maxHP} damage={this.props.boss.damage} />
+            </div>
+        )
+    }
+}
 
 export default App;
