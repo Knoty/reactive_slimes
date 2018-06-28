82c5074644d9355f1c92819b34bf67007a0ad066
Rodion Ziuzev <Knoty01@gmail.com>
2018-06-28 08:44:27 +0300

    Refactored code.

--- src/BossView.jsx
+++ src/BossView.jsx
@@ -3,7 +3,7 @@ import Boss from './Boss1.png';
 
 const BossView = props => (
     <div className="boss">
-        <button onClick={props.onClick}>
+        <button onClick = {props.onClick}>
             <img alt='Boss' src={Boss} />
         </button>
         <pre>{props.currentHP} / {props.maxHP}</pre>
--- src/CreateSlimeButton.jsx
+++ src/CreateSlimeButton.jsx
@@ -3,7 +3,7 @@ import add from './add.png';
 
 const CreateSlimeButton = props => (
     <div className="create_slime_button">
-        <button onClick={props.onClick}>
+        <button onClick = {props.onClick}>
             <img alt='Create new slime' src={add} />
         </button>
         <pre>{props.currentPoolAmount} / {props.maxPoolAmount}</pre>
