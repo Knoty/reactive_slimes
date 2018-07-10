54acb5be01f48012f1b5e4d462f7a6eab58c6b30
Rodion Ziuzev <Knoty01@gmail.com>
2018-07-11 01:29:15 +0300

    Replaced boss hp bar with level bar component.

--- src/BossView.jsx
+++ src/BossView.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Boss from './Boss1.png';
+import LevelBar from "./LevelBar";
+import PropTypes from 'prop-types';
 
 const BossView = props => (
     <div className="boss">
         <button onClick = {props.onClick}>
             <img alt='Boss' src={Boss} />
         </button>
-        <pre>{props.currentHP} / {props.maxHP}</pre>
+        <div className="level_bar_wrapper">
+            <LevelBar
+                current={props.currentHP}
+                max={props.maxHP}
+            />
+        </div>
     </div>
 );
+
+LevelBar.propTypes = {
+    currentHP: PropTypes.number.isRequired,
+    maxHP: PropTypes.number.isRequired,
+    onClick: PropTypes.func
+};
 
 export default BossView;
