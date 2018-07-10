a185fe8087aecdecb5c92e3c882109bf61509d57
Rodion Ziuzev <Knoty01@gmail.com>
2018-07-10 14:50:37 +0300

    Created hp bar component.

---
+++ src/LevelBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const getLevelBarLength = (currentLevel, maxLevel) => {
+    return (currentLevel / maxLevel) * 100
+};
+
+const LevelBar = props => (
+    <div className="level_bar">
+        <div
+            className="level_bar_value"
+            style={
+                {
+                    width: getLevelBarLength(props.current, props.max) + '%'
+                }
+            }
+        />
+    </div>
+);
+
+LevelBar.propTypes = {
+    current: PropTypes.number.isRequired,
+    max: PropTypes.number.isRequired,
+};
+
+export default LevelBar
