b8cfaca4bab91c5d9fe14a1a6d93be4c23ec82ee
Rodion Ziuzev <Knoty01@gmail.com>
2018-07-11 04:11:27 +0300

    Replaced hp bar for slimes with level bar component.

--- src/SlimeView.jsx
+++ src/SlimeView.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import poring from './pink_poring.png';
+import LevelBar from "./LevelBar";
+import PropTypes from 'prop-types';
 
 const SlimeView = props => (
     <div className={props.className}>
         <button onClick = {() => props.onClick(props.id)} >
             <img alt={`slime ${props.name}, id ${props.id}`} src={poring} />
         </button>
-        <pre>{props.hp} / {props.maxHP}</pre>
+        <div className="level_bar_wrapper">
+            <LevelBar
+                current={props.hp}
+                max={props.maxHP}
+            />
+        </div>
     </div>
 );
+
+LevelBar.propTypes = {
+    className: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+    name: PropTypes.any,
+    hp: PropTypes.number.isRequired,
+    maxHP: PropTypes.number.isRequired,
+};
 
 export default SlimeView
