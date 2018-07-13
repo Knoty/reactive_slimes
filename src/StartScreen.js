efbadae34c269f4e462b6ee9d86364c83811b785
Rodion Ziuzev <Knoty01@gmail.com>
2018-07-13 05:11:13 +0300

    Added css rules for start screen element.

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import StartButton from './start_button.png';
 import StartScreenBackground from './start_screen.png';
+import './StartScreen.css';
 
 const StartScreen = props => (
     <div className="start_screen">
-        <button className='start_button' autoFocus onClick={props.onClick}><img src={StartButton} alt="Начать игру" /></button>
+        <button className='start_button' autoFocus onClick={props.onClick}>
+            <img src={StartButton} alt="Начать игру" />
+        </button>
         <img src={StartScreenBackground} alt="Фон" />
     </div>
 );
